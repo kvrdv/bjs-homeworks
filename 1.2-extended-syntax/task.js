cfaf7e16942500df6e90962e3a7da6539db650b4
Semyon Kovardaev <s.kovardaev@gmail.com>
2020-04-06 16:32:35 +0300

    2nd attempt. Some fixes

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -2,18 +2,16 @@
 
 function getResult(a, b, c) {
 
-    let x;
+    let x = [];
     
-    let discr = Math.pow(b, 2) - 4*a*c;
+    let discriminant = Math.pow(b, 2) - 4*a*c;
 
-    if (discr < 0) {
-        x = [];
-    } else if (discr == 0) {
+    if (discriminant == 0) {
         x = [((-b) / 2 * a)];
-    } else if (discr > 0) {
+    } else if (discriminant > 0) {
         x = [
-            (((-b) + Math.sqrt(discr))/ 2 * a),
-            (((-b) - Math.sqrt(discr))/ 2 * a),
+            (((-b) + Math.sqrt(discriminant))/ 2 * a),
+            (((-b) - Math.sqrt(discriminant))/ 2 * a),
             ];
     };        
         
@@ -29,32 +27,26 @@ function getAverageMark(marks) {
     
     if (marks.length == 0) {
         return 0;
-    } else if (marks.length > 0 && marks.length <= 5) {
-        for (let i = 0; i <= marks.length; i++) {
-            sum = sum + marks[i];
-            return sum;
-        };
-        averageMark = (sum / marks.length);
-    } else (marks.length > 5); {
+    } else if (marks.length > 5); {
         console.log("Количество оценок больше 5");
         marks.splice(5);
-        averageMark = ((marks[0] + marks[1] + marks[2] + marks[3] + marks[4]) / marks.length);
     };
+    
+    for (let i = 0; i <= marks.length; i++) {
+        sum += marks[i];  
+        return averageMark = (sum / marks.length);
+    };   
 
-    return averageMark;
+    
 }
 
 function askDrink(name, dateOfBirthday) {
-    
-    let today = new Date();
 
-    let age = (today.getFullYear() - dateOfBirthday.getFullYear());
+    let age = (new Date().getFullYear() - dateOfBirthday.getFullYear());
 
     if (age >= 18) {
-        result = ("Не желаете ли олд - фэшн, " + name + "?");
+        return result = ("Не желаете ли олд - фэшн, " + name + "?");
     } else if (age < 18) {
-        result = ("Сожалею, " + name + " , но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!");
+        return result = ("Сожалею, " + name + " , но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!");
     };
-
-    return result;
 }
