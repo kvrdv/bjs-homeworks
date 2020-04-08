9b265f3e5a5fef82cae2b5d94e8b37e54b63d042
Semyon Kovardaev <s.kovardaev@gmail.com>
2020-04-08 14:28:59 +0300

    First commit

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,33 @@
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    
+    let body = (Number(amount) - Number(contribution));
+
+    let time = Math.round((date.getTime() - new Date().getTime()) / (3600 * 1000 * 24 * 30));
+
+    let payment = (body * ((percent / 1200) + ((percent / 1200) / (Math.pow((1 + (percent / 1200)), time) - 1))));
+
+    let totalAmount = (payment * time);
+
+    console.log(totalAmount.toFixed(2));
+
+    return totalAmount.toFixed(2);
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+
+    let greeting;
+
+    console.log(name);
+
+    if (Boolean(name) !== false) {
+        greeting = ("Привет, мир! Меня зовут " + name);
+        console.log(greeting);
+        return greeting;
+    } else {
+        greeting = ("Привет, мир! Меня зовут Аноним");
+        console.log(greeting);
+        return greeting;
+    };
 }
