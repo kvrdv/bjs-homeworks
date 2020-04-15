15a7524529c0674cb7d3ce16e01ec6a5cf582377
Semyon Kovardaev <s.kovardaev@gmail.com>
2020-04-15 20:51:02 +0300

    first commit

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,55 @@
+"use strict";
+
+// Задача 1 
+
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
-}
+
+  let sound = animal.sound;
+
+  if (animal == undefined) {
+    return null;
+  } else if (animal !== undefined) {
+    return sound;
+  };
+
+};
+
+// Задача 2
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
-}
+
+  let sum = 0,
+  averageMark = 0,
+  roundedAverage = 0;
+
+  console.log("Количество оценок: " + marks.length);
+
+  for (let i = 0; i < marks.length; i++) {
+    sum += Number(marks[i]);
+  };
+
+  averageMark = sum / marks.length;
+  roundedAverage = Math.round(averageMark);
+
+  return roundedAverage;
+
+};
+
+// Задача 3
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
-}
+
+  let age = Math.floor((Date.now(new Date()) - Date.parse(birthday)) / (3600 * 1000 * 24 * 30 * 12)),
+  verdict;
+
+  console.log(`The client is ${age} years old`);
+
+  if (age >= 18) {
+    verdict = true; 
+  } else if (age < 18) {
+    verdict = false;
+  };
+
+  return verdict;
+
+};
