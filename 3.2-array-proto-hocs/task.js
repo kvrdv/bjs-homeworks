a3e9c5c00fd41704dcba62c6a340e35cf16be1e7
Semyon Kovardaev <s.kovardaev@gmail.com>
2020-05-12 09:52:05 +0300

    second attempt

--- 3.2-array-proto-hocs/task.js
+++ 3.2-array-proto-hocs/task.js
@@ -19,19 +19,30 @@ function compareArrays(arr1, arr2) {
   }
 }
 
+compareArrays([8, 9], [6]);
+compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5]);
+compareArrays([9, 2, 4, 8, 2], [9, 2, 4]);
+compareArrays([1, 2, 3], [2, 3, 1]);
+compareArrays([8, 1, 2], [8, 1, 2]);
+
 function memorize(fn, limit) {
   let memory = [];
 
   return function (...args) {
     let found = memory.find(element => compareArrays(element.args, args));
+
     if (found) {
+      console.log('from memory');
       return found.result;
     }
+
     let result = fn(...args);
+
     memory.push({
       args: args,
       result: result
     });
+
     if (memory.length > limit) {
       memory.shift();
     }
@@ -40,7 +51,8 @@ function memorize(fn, limit) {
 }
 
 const mSum = memorize(sum, 5);
-
+console.log(mSum(6, 5));
+console.log(mSum(2, 5));
 const testArray = [[1, 2, 3], [1, 2], [1, 2, 3], [1, 2], [9, 5, 2, 4]];
 
 function testCase(testFunction, timer) {
