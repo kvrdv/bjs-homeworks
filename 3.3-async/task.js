d6e3586d24a164e701c1ba676ab2e9a25455188f
Semyon Kovardaev <s.kovardaev@gmail.com>
2020-05-13 07:40:39 +0300

    4th commit

--- 3.3-async/task.js
+++ 3.3-async/task.js
@@ -10,12 +10,12 @@ class AlarmClock {
 
   addClock(time, callback, id) {
     if (id === undefined) {
-      return console.error('Невозможно идентифицировать будильник. Параметр id не передан.');
+      return console.error("Невозможно идентифицировать будильник. Параметр id не передан.");
     }
 
     if (this.alarmCollection.length > 0) {
       if (this.alarmCollection.find(item => item.id === id)) {
-        return console.error('Будильник с таким id уже существует.');
+        return console.error("Будильник с таким id уже существует.");
       }
     }
 
@@ -33,7 +33,7 @@ class AlarmClock {
 
   getCurrentFormattedTime() {
     let time = new Date();
-    return time.getHours() + ':' + (time.getMinutes() < 10 ? '0' : '') + time.getMinutes();
+    return time.getHours() + ":" + (time.getMinutes() < 10 ? "0" : "") + time.getMinutes();
   }
 
   start() {
@@ -59,9 +59,9 @@ class AlarmClock {
   }
 
   printAlarms() {
-    console.log('Печать всех будильников в количестве: ' + this.alarmCollection.length)
+    console.log("Печать всех будильников в количестве: " + this.alarmCollection.length)
     this.alarmCollection.forEach((item) => {
-      console.log('Будильник № ' + item.id + ' заведен на ' + item.time);
+      console.log("Будильник № " + item.id + " заведен на " + item.time);
     });
   }
 
@@ -70,3 +70,16 @@ class AlarmClock {
     this.init();
   }
 }
+
+function testCase() {
+  const phoneAlarm = new AlarmClock();
+  phoneAlarm.addClock("20:01", () => console.log("Скоро спать"), 1);
+  phoneAlarm.addClock("20:02", () => { console.log("Пора готовиться ко сну!"); phoneAlarm.removeClock(2); }, 2);
+  phoneAlarm.addClock("20:03", () => console.log("Иди умываться"));
+  phoneAlarm.addClock("20:04", () => { console.log("Иди спать, завтра рано на работу!"); phoneAlarm.clearAlarms(); phoneAlarm.printAlarms(); }, 3);
+  phoneAlarm.addClock("21:05", () => console.log("Иди спать, завтра рано на работу!"), 1);
+  phoneAlarm.printAlarms();
+  phoneAlarm.start();
+}
+
+testCase();
